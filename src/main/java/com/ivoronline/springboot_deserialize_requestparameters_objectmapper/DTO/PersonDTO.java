3814383c9e8e8c7e6601945c916dfc53863b6b89
igoreta <ivoronline@gmail.com>
2021-03-15 09:01:37 +0100

    Initial commit

---
+++ src/main/java/com/ivoronline/springboot_deserialize_requestparameters_objectmapper/DTO/PersonDTO.java
@@ -0,0 +1,14 @@
+package com.ivoronline.springboot_deserialize_requestparameters_objectmapper.DTO;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+@JsonIgnoreProperties(ignoreUnknown = true)  //To avoid Error if Request contains additional Parameters
+public class PersonDTO {
+
+  @JsonProperty("firstName")                 //If Request Parameter and DTO Property have different names
+  public String name;
+
+  public String height;
+
+}
